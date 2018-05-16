8e9d9e4206eb25262d607a1f9d1fae6115b49b29
Jonathan Swindell <jes862000@gmail.com>
2018-05-16 09:43:16 -0500

    Game Balanced. Randomness added to the AI. Added subsequentBallDelay. Updated app to version 1.1.

--- UltmatePong/GameScene.swift
+++ UltmatePong/GameScene.swift
@@ -35,15 +35,19 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
     //3 = button pleyer won
     let userDefaults = UserDefaults.standard
     //gameplay tweaking varibles
+    let initalBallDelay = 1.5
+    let subsequentBallDelay = 0.85
     let ballVelocity:Int = 385
-    var playerControlDelay = 0.4
-    var easyAIDelay = 1.7
-    var mediumAIDelay = 1.45
-    var hardAIDelay = 1.3
-
+    var playerControlDelay = 0.45
+    var easyAIDelay = 1.075 //Balenced
+    var mediumAIDelay = 0.9 //Balenced
+    var hardAIDelay = 0.65 //Balenced
+    var minVariation = 0.0 //Dont change this without think through the consequences.
+    var maxVariation = 0.765 //Dont change this without think through the consequences.
+    
+    //Perfectly balenced like all things should be... quote - Thanos 2018
     
     override func didMove(to view: SKView) {
-        
         let ButtonTapSound = SKAction.playSoundFileNamed("Selection",waitForCompletion: false)
         run(ButtonTapSound)
         
@@ -56,8 +60,6 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         }else if(userDefaults.integer(forKey: "Mode")==3){
             currentGameType = gameType.player2
         }
-        
-        
         
         topLabel = self.childNode(withName: "topLabel") as! SKLabelNode
         bottomLabel = self.childNode(withName: "bottomLabel") as! SKLabelNode
@@ -82,9 +84,7 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         topLabel.text = "\(score[1])"
         bottomLabel.text = "\(score[0])"
         let DicideInpulseDirection = Int(arc4random_uniform(UInt32(4))) + 1
-        let when = DispatchTime.now() + 1 // change 1 to desired number of seconds
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            let timer = DispatchTime.now() + 0.5 // change 0.5 to desired number of seconds
+            let timer = DispatchTime.now() + initalBallDelay
             DispatchQueue.main.asyncAfter(deadline: timer) {
                 switch(DicideInpulseDirection){
                 case 1:
@@ -97,45 +97,48 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
                     self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: -self.ballVelocity))
                 default:
                     self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: self.ballVelocity))
-                }
-                
             }
         }
     }
-    
+
     func addScore(PlayerWhoWon: SKSpriteNode) {
         ball.position = CGPoint(x:0,y:0)
         ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
         let DicideInpulseDirection = Int(arc4random_uniform(UInt32(4))) + 1
         if PlayerWhoWon == main{
             score[0] += 1
+            let timer = DispatchTime.now() + subsequentBallDelay
+            DispatchQueue.main.asyncAfter(deadline: timer) {
             switch(DicideInpulseDirection){
             case 1:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: self.ballVelocity))
             case 2:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: -ballVelocity, dy: -ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: -self.ballVelocity, dy: -self.ballVelocity))
             case 3:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: -ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: -self.ballVelocity, dy: self.ballVelocity))
             case 4:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: -ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: -self.ballVelocity))
             default:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: self.ballVelocity))
+                }
             }
         }else if PlayerWhoWon == enemy{
             score[1] += 1
+            let timer = DispatchTime.now() + subsequentBallDelay
+            DispatchQueue.main.asyncAfter(deadline: timer) {
             switch(DicideInpulseDirection){
             case 1:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: self.ballVelocity))
             case 2:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: -ballVelocity, dy: -ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: -self.ballVelocity, dy: -self.ballVelocity))
             case 3:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: -ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: -self.ballVelocity, dy: self.ballVelocity))
             case 4:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: -ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: -self.ballVelocity))
             default:
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: ballVelocity, dy: ballVelocity))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: self.ballVelocity, dy: self.ballVelocity))
+                }
             }
-            
         }
         topLabel.text = "\(score[1])"
         bottomLabel.text = "\(score[0])"
@@ -171,15 +174,16 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
     
   
     override func update(_ currentTime: TimeInterval) {
+        var randomVariation = randomDouble(min: minVariation, max: maxVariation)
         switch currentGameType {
         case .easy:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: easyAIDelay))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: easyAIDelay + randomVariation))
             break
         case .medium:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: mediumAIDelay))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: mediumAIDelay + randomVariation))
             break
         case .hard:
-            enemy.run(SKAction.moveTo(x: ball.position.x, duration: mediumAIDelay))
+            enemy.run(SKAction.moveTo(x: ball.position.x, duration: mediumAIDelay + randomVariation))
             break
         case .player2:
             
@@ -229,7 +233,8 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
             NextScreen?.scaleMode = .aspectFill
             self.scene?.view?.presentScene(NextScreen!)
         }
-        
-        
+    }
+    func randomDouble(min: Double, max: Double) -> Double {
+        return (Double(arc4random()) / 0xFFFFFFFF) * (max - min) + min
     }
 }
